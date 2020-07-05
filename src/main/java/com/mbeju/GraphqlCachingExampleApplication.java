683b70e39faa94232d5923f7ac8dc8f7a700234d
lhmzhou <lhmzhou@gmail.com>
2020-07-05 10:40:26 -0400

    wrap cache operations within caffeine

--- src/main/java/com/mbeju/GraphqlCachingExampleApplication.java
+++ src/main/java/com/mbeju/GraphqlCachingExampleApplication.java
@@ -1,9 +1,11 @@
 package com.mbeju;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
@@ -20,13 +22,23 @@ public class GraphqlCachingExampleApplication {
 	@Primary
 	@Bean(value = "producerCache")
 	public CacheManager producerCacheManager() {
-	
+		CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+		cacheManager.setCaffeine(caffineCacheBuilder(20));
+		return cacheManager;
 	}
 
 	@Bean(value = "postCache")
 	public CacheManager postCacheManager() {
-
+		CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+		cacheManager.setCaffeine(caffineCacheBuilder(5));
+		return cacheManager;
 	}
 
+	Caffeine<Object, Object> caffineCacheBuilder(int ttl) {
+		return Caffeine.newBuilder()
+				.initialCapacity(10)
+				.maximumSize(10)
+				.expireAfterWrite(Duration.ofSeconds(ttl))
+				.recordStats();
+	}
 }
-
